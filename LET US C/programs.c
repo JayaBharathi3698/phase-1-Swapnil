550ce1892b324f9c128055e9cd731fba6ccab3d1
SWAPNIL-SAGAR <85029234+SwapnilSagar@users.noreply.github.com>
2021-09-01 10:33:08 +0530

    Update programs.c

--- LET US C/programs.c
+++ LET US C/programs.c
@@ -25,3 +25,41 @@ void main()
 	
 	  printf("sum of number is = %d",temp);
 }
+
+Q2 :- WAP to merge 2 given array by using 3rd array.
+	
+#include<stdio.h>
+void main()
+{
+
+    int a[6]={1,3,5,7,9,11};
+    int b[6]={2,4,6,8,10,12};
+    int c[12];
+    
+    int i,j,k;
+    
+    for(i=0,j=0,k=0;i<6,j<6,k<12;)
+    {
+    	if(a[i]<b[j])
+    	{
+    		c[k]=a[i];
+    		i++;
+    		k++;
+		}
+		else 
+		if(b[j]<a[i])
+		{
+			c[k]=b[j];
+			j++;
+			k++;
+			
+		}
+    	
+    }
+	printf("resultant array is ");
+	for(k=0;k<12;k++)
+	{
+		printf("%d\n",c[k]);
+	}
+    
+}
