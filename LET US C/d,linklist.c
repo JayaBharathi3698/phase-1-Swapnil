b8a3f9dc229b88c2d5dd6efb7c0539c6d0e596fe
SWAPNIL-SAGAR <sagswap88@gmail.com>
2021-10-08 18:49:55 +0530

    Create d,linklist.c

---
+++ LET US C/d,linklist.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node 
+{
+    int x;
+    struct node *next;
+
+};
+
+struct node *head=NULL;
+
+void insert(int val)
+{
+    struct node *temp;
+    temp=(struct node*)malloc(sizeof(struct node));
+    temp->x=val;
+    temp->next=head;
+    head=temp;
+
+}
+
+void main()
+{
+    char c;
+    int i;
+    while(c!='n')
+    {
+        printf("Enter value \n");
+        scanf("%d",&i);
+        insert(i);
+        printf("\n Do you want to add more press y or n\n");
+        scanf("%c",&c);
+    }
+    struct node *run;
+    run=head;
+    while(run!=NULL)
+    {
+        printf("\n%d\n",run->x);
+        run=run->next;
+    }
+}
